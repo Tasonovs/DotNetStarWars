a9e661084641a480072eb9fcae91a4a9c0309867
Stanislav Shvyryov <ctac00025@gmail.com>
2023-06-02 21:57:49 +0300

    [init] Add necessary infrastructure

---
+++ DotNetStarWars.Domain/StarWarsContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetStarWars.Domain;
+
+public class StarWarsContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public StarWarsContext(DbContextOptions<StarWarsContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public override int SaveChanges()
+    {
+        ChangeTracker.DetectChanges();
+
+        // update JSON file .GetAwaiter().GetResult();
+
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        ChangeTracker.DetectChanges();
+
+        // update JSON file 
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+}
--- DotNetStarWars/Program.cs
+++ DotNetStarWars/Program.cs
@@ -1,6 +1,11 @@
+using DotNetStarWars.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StarWarsContext>(options => options.UseSqlite(dbConnectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,14 +13,24 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+ApplyMigrations(app);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseStaticFiles();
+app.UseRouting();
 
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+
+void ApplyMigrations(WebApplication application)
+{
+    using var scope = application.Services.CreateScope();
+    var erpContext = scope.ServiceProvider.GetRequiredService<StarWarsContext>();
+    erpContext.Database.Migrate();
+}
